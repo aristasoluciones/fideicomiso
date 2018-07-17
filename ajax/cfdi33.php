5e99ccb1e54629a657e84cd8868e376d2752c868
Arista Soluciones <isc061990@outlook.com>
2018-07-16 22:16:21 -0500

    bad

--- ajax/cfdi33.php
+++ ajax/cfdi33.php
@@ -166,7 +166,7 @@ switch($_POST["type"])
         {
             $array = explode("=", $values[$key]);
             if($array[0]=='razonSocial')
-                $data[$array[0]] = utf8_encode($array[1]);
+                $data[$array[0]] = utf8_decode(htmlspecialchars($array[1]));
             else
                 $data[$array[0]] = $array[1];
         }
